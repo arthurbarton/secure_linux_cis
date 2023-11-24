b7e7512e096d1481fc51d124a1b1c5636c60339f
Arthur Barton (arthurbarton) <arthurb@igniferous.net>
2023-11-24 11:10:41 +1100

    ignore that

--- manifests/rules/ensure_sudo_is_installed.pp
+++ manifests/rules/ensure_sudo_is_installed.pp
@@ -3,10 +3,7 @@
 # @summary Ensure sudo is installed 
 #
 class secure_linux_cis::rules::ensure_sudo_is_installed {
-  # We want CIS to ensure sudo, but its often installed elsewhere
-  if !defined(Package['sudo']) {
-    package { 'sudo':
-      ensure   => installed,
-    }
+  package { 'sudo':
+    ensure   => installed,
   }
 }
