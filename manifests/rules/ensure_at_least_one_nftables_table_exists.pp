1425d6fdf05e9923990843bbc91bc939895ee548
Arthur Barton (arthurbarton) <arthurb@igniferous.net>
2024-07-04 15:26:25 +1000

    include required script

--- manifests/rules/ensure_at_least_one_nftables_table_exists.pp
+++ manifests/rules/ensure_at_least_one_nftables_table_exists.pp
@@ -3,8 +3,17 @@
 # @summary Ensure at least one nftables table exists
 #
 class secure_linux_cis::rules::ensure_at_least_one_nftables_table_exists {
-  exec { "Ensure at least one nftables table exists":
-    command   => "/usr/share/cis_scripts/ensure_at_least_one_nftables_table_exists.sh",
+  file { '/usr/share/cis_scripts/ensure_at_least_one_nftables_table_exists.sh':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    content => file('secure_linux_cis/ensure_at_least_one_nftables_table_exists.sh'),
+    before  => Exec['Ensure at least one nftables table exists'],
+  }
+
+  exec { 'Ensure at least one nftables table exists':
+    command   => '/usr/share/cis_scripts/ensure_at_least_one_nftables_table_exists.sh',
     logoutput => true,
   }
 }
