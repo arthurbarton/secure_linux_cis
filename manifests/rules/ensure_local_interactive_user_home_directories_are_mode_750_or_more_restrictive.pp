de078f74e51ae0463166a81de542f8e6ab6b976e
Arthur Barton (arthurbarton) <arthurb@igniferous.net>
2024-07-04 15:24:07 +1000

    include required script

--- manifests/rules/ensure_local_interactive_user_home_directories_are_mode_750_or_more_restrictive.pp
+++ manifests/rules/ensure_local_interactive_user_home_directories_are_mode_750_or_more_restrictive.pp
@@ -3,8 +3,16 @@
 # @summary Ensure local interactive user home directories are mode 750 or more restrictive
 #
 class secure_linux_cis::rules::ensure_local_interactive_user_home_directories_are_mode_750_or_more_restrictive {
-  exec { "Ensure local interactive user home directories are mode 750 or more restrictive":
-    command   => "/usr/share/cis_scripts/ensure_local_interactive_user_home_directories_are_mode_750_or_more_restrictive.sh",
+  file { '/usr/share/cis_scripts/ensure_local_interactive_user_home_directories_are_mode_750_or_more_restrictive.sh':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    content => file('secure_linux_cis/ensure_local_interactive_user_home_directories_are_mode_750_or_more_restrictive.sh'),
+    before  => Exec['Ensure local interactive user home directories are mode 750 or more restrictive'],
+  }
+  exec { 'Ensure local interactive user home directories are mode 750 or more restrictive':
+    command   => '/usr/share/cis_scripts/ensure_local_interactive_user_home_directories_are_mode_750_or_more_restrictive.sh',
     logoutput => true,
   }
 }
