fa0c86d0c75809bf4d7a2b921bba592ab8c389ca
Arthur Barton (arthurbarton) <arthurb@igniferous.net>
2024-07-11 15:09:28 +1000

    before exec

--- manifests/rules/ensure_host_based_firewall_loopback_traffic_is_configured.pp
+++ manifests/rules/ensure_host_based_firewall_loopback_traffic_is_configured.pp
@@ -3,6 +3,14 @@
 # @summary Ensure host based firewall loopback traffic is configured
 #
 class secure_linux_cis::rules::ensure_host_based_firewall_loopback_traffic_is_configured {
+  file { '/usr/share/cis_scripts/ensure_host_based_firewall_loopback_traffic_is_configured.sh':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    content => file('secure_linux_cis/ensure_host_based_firewall_loopback_traffic_is_configured.sh'),
+    before  => Exec['Ensure host based firewall loopback traffic is configured'],
+  }
   exec { "Ensure host based firewall loopback traffic is configured":
     command   => "/usr/share/cis_scripts/ensure_host_based_firewall_loopback_traffic_is_configured.sh",
     logoutput => true,
