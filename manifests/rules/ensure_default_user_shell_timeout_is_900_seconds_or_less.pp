d6094a36fdf52884537afcadfd2584ad6120af5e
Arthur Barton (arthurbarton) <arthurb@igniferous.net>
2024-07-11 15:10:45 +1000

    before exec

--- manifests/rules/ensure_default_user_shell_timeout_is_900_seconds_or_less.pp
+++ manifests/rules/ensure_default_user_shell_timeout_is_900_seconds_or_less.pp
@@ -3,6 +3,14 @@
 # @summary Ensure default user shell timeout is 900 seconds or less
 #
 class secure_linux_cis::rules::ensure_default_user_shell_timeout_is_900_seconds_or_less {
+  file { '/usr/share/cis_scripts/ensure_default_user_shell_timeout_is_900_seconds_or_less.sh':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    content => file('secure_linux_cis/ensure_default_user_shell_timeout_is_900_seconds_or_less.sh'),
+    before  => Exec['Ensure default user shell timeout is 900 seconds or less'],
+  }
   exec { "Ensure default user shell timeout is 900 seconds or less":
     command   => "/usr/share/cis_scripts/ensure_default_user_shell_timeout_is_900_seconds_or_less.sh",
     logoutput => true,
