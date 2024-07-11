b6e7162c573f9476ddc90b4eb44a20d9311efdb1
Arthur Barton (arthurbarton) <arthurb@igniferous.net>
2024-07-11 15:00:07 +1000

    before exec

--- manifests/rules/ensure_password_hashing_algorithm_is_sha_512_or_yescrypt.pp
+++ manifests/rules/ensure_password_hashing_algorithm_is_sha_512_or_yescrypt.pp
@@ -7,6 +7,14 @@ class secure_linux_cis::rules::ensure_password_hashing_algorithm_is_sha_512_or_y
     String $encrypt_method_libuser_conf = 'sha512',
 
 ) {
+  file { '/usr/share/cis_scripts/ensure_a_single_firewall_configuration_utility_is_in_use.sh':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    content => file('secure_linux_cis/ensure_a_single_firewall_configuration_utility_is_in_use.sh'),
+    before  => Exec['Ensure a single firewall configuration utility is in use'],
+  }
   file_line { 'Ensure ENCRYPT_METHOD set properly':
     path  => '/etc/login.defs',
     match => '^\s*ENCRYPT_METHOD\s+',
