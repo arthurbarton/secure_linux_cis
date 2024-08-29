da9799ca2507b0be8e4f9b332db765c4c61df26f
Arthur Barton (arthurbarton) <arthurb@igniferous.net>
2024-08-30 09:34:01 +1000

    extra tests

--- files/ensure_authselect_includes_with_faillock.sh
+++ files/ensure_authselect_includes_with_faillock.sh
@@ -1,2 +1,4 @@
-
-/usr/bin/authselect enable-feature with-faillock && /usr/bin/authselect apply-changes
+authselect_status=$(/usr/bin/authselect current --raw)
+if [[ ! $authselect_status =~ "with-faillock" ]]; then
+        /usr/bin/authselect enable-feature with-faillock && /usr/bin/authselect apply-changes
+fi
