fda45a0ffe20ad3aef0bec0c24bbafec40855153
Arthur Barton (arthurbarton) <arthurb@igniferous.net>
2024-10-02 09:11:04 +1000

    may be managed already

--- manifests/rules/ensure_journald_service_is_enabled.pp
+++ manifests/rules/ensure_journald_service_is_enabled.pp
@@ -3,8 +3,10 @@
 # @summary Ensure journald service is enabled 
 #
 class secure_linux_cis::rules::ensure_journald_service_is_enabled {
-  service { 'systemd-journald':
-    ensure => running,
-    enable => true,
+  if !defined(Service['systemd-journald']) {
+    service { 'systemd-journald':
+      ensure => running,
+      enable => true,
+    }
   }
 }
